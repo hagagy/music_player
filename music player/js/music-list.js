801c6c9ab9cf54033520c00ef616d233b1b9655f
hagagy <66977104+hagagy@users.noreply.github.com>
2021-08-29 17:19:51 +0200

    Add files via upload

---
+++ music player/js/music-list.js
@@ -0,0 +1,40 @@
+let allMusic = 
+    [
+ {
+    name: "Scoo Scoo",
+    artist: "Abo El Anwar",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Bad Liar",
+    artist: "Imagine Dragons",
+    img: "music-2",
+    src: "music-2"
+  },
+ 
+  {
+    name: "I'm Still Here",
+    artist: "Sia",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Sheraton",
+    artist: "Marwan Moussa",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "We Can't Stop",
+    artist: "Miley Cyrus ",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Asyad El Soot",
+    artist: "Wegz",
+    img: "music-6",
+    src: "music-6"
+  },
+];
